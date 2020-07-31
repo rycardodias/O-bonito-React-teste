391db236dae1da043119ef98a65ed7e2befd863a
Ricardo Dias <rycardo.dias@hotmail.com>
2020-07-31 17:53:30 +0100

    commit on master

--- src/components/Cozinha.js
+++ src/components/Cozinha.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react'
 
 export default class Cozinha extends Component {
+    state = {
+        buttonIndex: null
+    }
+
+    _handleClick = (e) => {
+        this.setState({
+            buttonIndex: e.target.id
+        })
+    }
+
     render() {
         const modulos = ['Gestão Stock', 'Refeições', 'Ementas', 'Gestão Limpeza', 'Histórico Refeições', 'Horários']
 
-        const item = modulos.map(
-            (modulo) =>
+        const itens = modulos.map(
+            (modulo, index) =>
                 <div class="column is-one-third">
-                    <button style={{ width: '100%', height: '100%' }}>{modulo}</button>
+                    <button
+                        id={index}
+                        onClick={this._handleClick}
+                        style={{ width: '100%', height: '100%' }}>{modulo}</button>
                 </div>
         )
+
         return (
             <div class="columns is-multiline is-mobile">
-                {item}
+                
             </div>
         )
     }
